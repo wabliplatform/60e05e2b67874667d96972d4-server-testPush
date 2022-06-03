9fb4c05571c10b2cc02187e129529aeb09373512
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-06-03 09:45:39 +0300

    My commit message

---
+++ models/Form.js
@@ -0,0 +1,18 @@
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const formSchema = new Schema({
+Underscoreid:String , 
+
+
+formName:String 
+
+
+
+})
+
+module.exports = {
+  Form : mongoose.model('form', formSchema),
+}
+
